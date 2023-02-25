09a6d00f1e29862c743556ebbdeba0ffbbb6aee2
Sam Birch <sambirch@sams-mbp.home>
2023-02-25 20:19:21 +0000

    It's a beautiful day

--- .config/nvim/lua/sambirch/init.lua
+++ .config/nvim/lua/sambirch/init.lua
@@ -2,7 +2,4 @@ require("sambirch.set")
 require("sambirch.remap")
 require("sambirch.neogit")
 
-vim.cmd([[
-let g:netrw_bufsettings="noma nomod nonu nobl nowrap ro rnu"
-]])
 -- print("hello from sambirch")
--- .config/nvim/lua/sambirch/set.lua
+++ .config/nvim/lua/sambirch/set.lua
@@ -17,3 +17,16 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.g.mapleader = " "
+
+vim.cmd [[
+let g:netrw_bufsettings="noma nomod nonu nobl nowrap ro rnu"
+]]
+
+-- This tells vim/nvim to use the system clipboard for all yank, delete, change and put operations
+vim.cmd [[
+if has("unnamedplus")
+    set clipboard=unnamedplus
+else
+    set clipboard=unnamed
+endif
+]]
